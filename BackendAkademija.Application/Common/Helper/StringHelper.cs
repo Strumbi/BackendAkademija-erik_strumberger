f6483024659b8cc3fe88673b2869e8ede82e624d
Strumbi <92187495+Strumbi@users.noreply.github.com>
2026-06-30 17:29:31 +0200

    Truncate
    Popravljeno da u slucaju da je prosljedena vrijenost null da ne baci NullRefrenceException

--- BackendAkademija.Application/Common/Helper/StringHelper.cs
+++ BackendAkademija.Application/Common/Helper/StringHelper.cs
@@ -2,5 +2,8 @@ namespace BackendAkademija.Application.Helper;
 
 public static class StringHelper
 {
-    public static string Truncate(string value, int maxLength) => value.Length <= maxLength ? value : value[..maxLength] + "...";
+    public static string Truncate(string? value, int maxLength) => 
+    string.IsNullOrEmpty(value) || value.Length <= maxLength 
+        ? value ?? string.Empty 
+        : value[..maxLength] + "...";
 }
